bf71eab612af5d717027e46edb90c17573f7b01c
Omarahorrar5 <omarahorar@gmail.com>
2026-09-25 17:58:52 +0000

    adding sg ingress rule for web traffic at port 80

--- main.tf
+++ main.tf
@@ -133,6 +133,14 @@ resource "aws_security_group" "ec2" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_block  = ["0.0.0.0/0"]
+  }
+
   # Outbound traffic
   egress {
     description = "Allow all outbound traffic"
